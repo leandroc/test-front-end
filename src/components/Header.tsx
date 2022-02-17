2fac7ce1719f34f8bb441dfba0f25aa357f3ac23
Leandro Araújo <leandro.cesar.ma@gmail.com>
2022-02-16 22:04:22 -0300

    Updates

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Header.module.css";
-
-export const Header = () => {
-  return (
-    <header className={styles.container}>
-      <h1 className={styles.title}>User search</h1>
-    </header>
-  );
-};
